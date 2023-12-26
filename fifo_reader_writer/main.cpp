8ffd1305206de2e8e8417724ffa3236fe503d805
Alexander Gureshidze <alekogureshidze@gmail.com>
2023-12-26 20:28:20 +0400

    Initial import

---
+++ fifo_reader_writer/main.cpp
@@ -0,0 +1,47 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <fstream>
+#include <iostream>
+int main()
+{
+      std::ifstream file("physics-2023s-biggest-breakthroughs-youtubemp3free.org.wav", std::ios::binary); // Open the file in binary mode
+
+
+    const char *fifoPath = "/home/user/virtual_mic/virtual_mic"; // Replace with the path to your FIFO
+
+    int fd = open(fifoPath, O_WRONLY); // Open the FIFO for writing
+	if (fd == -1) 
+	{
+		std::cout << "Failed to open fifo" << std::endl;
+		exit(1);
+	}
+
+    if (file.is_open()) {
+
+
+
+	while(true)
+{
+        // Read a specified number of bytes into a buffer
+        const int BUFFER_SIZE = 320;
+        char buffer[BUFFER_SIZE];
+        file.read(buffer, BUFFER_SIZE);
+
+        // Get the actual number of bytes read
+        std::streamsize bytesRead = file.gcount();
+
+	std::cout << "Try to write bytes" << std::endl;
+        // Process the content of the buffer here
+ssize_t bytesWritten = write(fd, buffer, BUFFER_SIZE);
+if (bytesWritten == -1) {
+	std::cout << "error writing into fifo " << std::endl;
+	exit(2);
+}
+
+
+}
+        file.close(); // Close the file when done
+    } else {
+        std::cerr << "Error opening the file." << std::endl;
+    }
+}
