567194eacb8de8aaeff79cde5d865a2465c507d5
Alexander <alekogureshidze@gmail.com>
2023-12-27 12:03:34 +0400

    pass input audio from command line

--- fifo_reader_writer/main.cpp
+++ fifo_reader_writer/main.cpp
@@ -2,9 +2,9 @@
 #include <unistd.h>
 #include <fstream>
 #include <iostream>
-int main()
+int main(int argc, char** argv)
 {
-      std::ifstream file("../physics-2023s-biggest-breakthroughs-youtubemp3free.org.wav", std::ios::binary); // Open the file in binary mode
+      std::ifstream file(argv[1], std::ios::binary); // Open the file in binary mode
 
 
     const char *fifoPath = "/tmp/virtual_mic"; // Replace with the path to your FIFO
@@ -17,33 +17,28 @@ int main()
 	}
 
     if (file.is_open()) {
+	    while(true)
+        {
+            // Read a specified number of bytes into a buffer
+            const int BUFFER_SIZE = 882/*320*/;
+            char buffer[BUFFER_SIZE];
+            file.read(buffer, BUFFER_SIZE);
 
+            // Get the actual number of bytes read
+            std::streamsize bytesRead = file.gcount();
 
-
-	while(true)
-{
-        // Read a specified number of bytes into a buffer
-        const int BUFFER_SIZE = 882/*320*/;
-        char buffer[BUFFER_SIZE];
-        file.read(buffer, BUFFER_SIZE);
-
-        // Get the actual number of bytes read
-        std::streamsize bytesRead = file.gcount();
-
-	std::cout << "Try to write bytes" << std::endl;
-        // Process the content of the buffer here
-ssize_t bytesWritten = write(fd, buffer, BUFFER_SIZE);
-if (bytesWritten == -1) {
-	std::cout << "error writing into fifo " << std::endl;
-	exit(2);
-}
-else
-{
-    usleep(10000);
-    }
-
-
-}
+	        std::cout << "Try to write bytes" << std::endl;
+            // Process the content of the buffer here
+            ssize_t bytesWritten = write(fd, buffer, BUFFER_SIZE);
+            if (bytesWritten == -1) {
+	            std::cout << "error writing into fifo " << std::endl;
+	            exit(2);
+            }
+            else
+            {
+                usleep(10000);
+            }
+        }
         file.close(); // Close the file when done
     } else {
         std::cerr << "Error opening the file." << std::endl;
